c8f802e393fd135242115d9539a4dd11b7f86063
wujia <wzsayiie@qq.com>
2023-05-03 23:26:01 +0800

    build settings.

--- Assets/CSharp/Editor/BuildSettings/BuildSettings.cs
+++ Assets/CSharp/Editor/BuildSettings/BuildSettings.cs
@@ -1,5 +1,38 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-class BuildSettings : ScriptableObject
+namespace U3DMobileEditor
 {
+    //bundle entry:
+    [Serializable]
+    internal enum PackStrategy
+    {
+        Entire  = 0,
+        SubDir  = 1,
+        SubFile = 2,
+    }
+
+    [Serializable]
+    internal enum RuntimeDemand
+    {
+        Necessary = 0,
+        OnDemand  = 1,
+    }
+
+    [Serializable]
+    internal class BundleEntry
+    {
+        public UnityEngine.Object file    ;
+        public PackStrategy       strategy;
+        public RuntimeDemand      demand  ;
+        public string             carryOpt;
+    }
+
+    //build settings.
+    internal class BuildSettings : ScriptableObject
+    {
+        [SerializeField]
+        List<BundleEntry> _entries;
+    }
 }
--- Assets/CSharp/Editor/BuildSettings/BuildSettingsInspector.cs
+++ Assets/CSharp/Editor/BuildSettings/BuildSettingsInspector.cs
@@ -1,6 +1,71 @@
+using U3DMobile;
 using UnityEditor;
+using UnityEngine;
 
-[CustomEditor(typeof(BuildSettings))]
-class BuildSettingsInpector : Editor
+namespace U3DMobileEditor
 {
+    [CustomPropertyDrawer(typeof(BundleEntry))]
+    internal class BundleEntryDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight * 2;
+        }
+
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            //first line:
+            Rect _1 = new Rect(r.x      , r.y, 110          , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x + 115, r.y,  65          , EditorGUIUtility.singleLineHeight);
+            Rect _3 = new Rect(r.x + 185, r.y,  80          , EditorGUIUtility.singleLineHeight);
+            Rect _4 = new Rect(r.x + 270, r.y, r.width - 270, EditorGUIUtility.singleLineHeight);
+
+            SerializedProperty file = property.FindPropertyRelative("file");
+            EditorGUI.PropertyField(_1, file, GUIContent.none);
+
+            UnityEngine.Object fObj = file.objectReferenceValue;
+            EditorGUI.BeginDisabledGroup(fObj == null);
+
+            EditorGUI.PropertyField(_2, property.FindPropertyRelative("strategy"), GUIContent.none);
+            EditorGUI.PropertyField(_3, property.FindPropertyRelative("demand"  ), GUIContent.none);
+            EditorGUI.PropertyField(_4, property.FindPropertyRelative("carryOpt"), GUIContent.none);
+
+            EditorGUI.EndDisabledGroup();
+
+            //second line:
+            Rect _6 = new Rect(
+                r.x    , EditorGUIUtility.singleLineHeight + r.y,
+                r.width, EditorGUIUtility.singleLineHeight
+            );
+            if (fObj != null)
+            {
+                EditorGUI.LabelField(_6, AssetDatabase.GetAssetPath(fObj));
+            }
+        }
+    }
+
+    [CustomEditor(typeof(BuildSettings))]
+    internal class BuildSettingsInspector : Editor
+    {
+        private SerializedProperty _entries;
+
+        internal static BuildSettingsInspector instance;
+
+        private void OnEnable()
+        {
+            instance = this;
+
+            _entries = serializedObject.FindProperty("_entries");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            //bundle entries:
+            EditorGUILayout.PropertyField(_entries, new GUIContent("Bundle Entries"));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
