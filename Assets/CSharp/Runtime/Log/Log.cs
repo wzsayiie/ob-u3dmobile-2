393c1e5673f0923adee02c5492b88302112ee0cf
wujia <wzsayiie@qq.com>
2023-05-03 23:23:40 +0800

    log manager.

---
+++ Assets/CSharp/Runtime/Log/Log.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace U3DMobile
+{
+    public static class Log
+    {
+        public static void Info(string format, params object[] objects)
+        {
+            Debug.LogFormat(format, objects);
+        }
+
+        public static void Error(string format, params object[] objects)
+        {
+            Debug.LogErrorFormat(format, objects);
+        }
+
+        public static void InfoString(string text)
+        {
+            Debug.Log(text);
+        }
+
+        public static void ErrorString(string text)
+        {
+            Debug.LogError(text);
+        }
+    }
+}
