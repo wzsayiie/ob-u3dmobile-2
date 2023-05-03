ec817e9aebff9a0333b4ac3e79cd0fe2346f7346
wujia <wzsayiie@qq.com>
2023-05-03 23:25:13 +0800

    game settings.

--- Assets/CSharp/Editor/GameSettings/GameSettingsInpector.cs
+++ Assets/CSharp/Editor/GameSettings/GameSettingsInpector.cs
@@ -1,7 +1,182 @@
 using U3DMobile;
 using UnityEditor;
+using UnityEngine;
 
-[CustomEditor(typeof(GameSettings))]
-class GameSettingsInpector : Editor
+namespace U3DMobileEditor
 {
+    [CustomPropertyDrawer(typeof(PackageIdentifier))]
+    internal class PackageIdentifierDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            Rect _1 = new Rect(r.x      , r.y, 110          , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x + 110, r.y, r.width - 110, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.LabelField(_1, "Package Identifier");
+            EditorGUI.PropertyField(_2, property.FindPropertyRelative("iden"), GUIContent.none);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(StoreChannel))]
+    internal class StoreChannelDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            Rect _1 = new Rect(r.x      , r.y, 20           , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x +  20, r.y, 90           , EditorGUIUtility.singleLineHeight);
+            Rect _3 = new Rect(r.x + 120, r.y, 60           , EditorGUIUtility.singleLineHeight);
+            Rect _4 = new Rect(r.x + 180, r.y, r.width - 180, EditorGUIUtility.singleLineHeight);
+
+            SerializedProperty channel = property.FindPropertyRelative("channel");
+
+            string current = channel.stringValue;
+            string active  = GameSettingsInspector.instance.activeChan;
+            bool   oldIsOn = (
+                !string.IsNullOrWhiteSpace(current) &&
+                !string.IsNullOrWhiteSpace(active ) &&
+                current.Trim() == active.Trim()
+            );
+
+            //use bold style to remind users that this is a "radio" toggle.
+            bool newIsOn = EditorGUI.Toggle(_1, oldIsOn, GUI.skin.GetStyle("BoldToggle"));
+            if (!oldIsOn && newIsOn)
+            {
+                GameSettingsInspector.instance.activeChan = current;
+            }
+            EditorGUI.PropertyField(_2, channel, GUIContent.none);
+
+            EditorGUI.LabelField(_3, "Gateway");
+            EditorGUI.PropertyField(_4, property.FindPropertyRelative("gateway"), GUIContent.none);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(ForcedUrlItem))]
+    internal class ForcedUrlsDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            Rect _1 = new Rect(r.x      , r.y, 20           , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x +  20, r.y, 80           , EditorGUIUtility.singleLineHeight);
+            Rect _3 = new Rect(r.x + 100, r.y, r.width - 100, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.PropertyField(_1, property.FindPropertyRelative("enabled"), GUIContent.none);
+
+            switch (property.name)
+            {
+                case "asset": EditorGUI.LabelField(_2, "Asset URL"); break;
+                case "patch": EditorGUI.LabelField(_2, "Patch URL"); break;
+            }
+            EditorGUI.PropertyField(_3, property.FindPropertyRelative("url"), GUIContent.none);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(AssetFlavor))]
+    internal class AssetFlavorDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+	    {
+            Rect _1 = new Rect(r.x        , r.y, 40           , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x    + 40, r.y, r.width - 120, EditorGUIUtility.singleLineHeight);
+            Rect _3 = new Rect(r.xMax - 70, r.y, 50           , EditorGUIUtility.singleLineHeight);
+            Rect _4 = new Rect(r.xMax - 20, r.y, 20           , EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.LabelField(_1, "Flavor");
+            EditorGUI.PropertyField(_2, property.FindPropertyRelative("name"), GUIContent.none);
+
+            EditorGUI.LabelField(_3, "Enabled");
+            EditorGUI.PropertyField(_4, property.FindPropertyRelative("enabled"), GUIContent.none);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(UserFlag))]
+    internal class UserFlagDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            Rect _1 = new Rect(r.x      , r.y, 90           , EditorGUIUtility.singleLineHeight);
+            Rect _2 = new Rect(r.x + 100, r.y, 60           , EditorGUIUtility.singleLineHeight);
+            Rect _3 = new Rect(r.x + 170, r.y, 20           , EditorGUIUtility.singleLineHeight);
+            Rect _4 = new Rect(r.x + 170, r.y, r.width - 170, EditorGUIUtility.singleLineHeight);
+            Rect _5 = new Rect(r.x + 170, r.y, r.width - 170, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.PropertyField(_1, property.FindPropertyRelative("name"), GUIContent.none);
+
+            SerializedProperty type = property.FindPropertyRelative("type");
+            EditorGUI.PropertyField(_2, type, GUIContent.none);
+
+            switch ((UserFlagType)type.enumValueFlag)
+            {
+                case UserFlagType.Bool: {
+                    EditorGUI.PropertyField(_3, property.FindPropertyRelative("boolValue"), GUIContent.none);
+                    break;
+                }
+                case UserFlagType.Int: {
+                    EditorGUI.PropertyField(_4, property.FindPropertyRelative("intValue"), GUIContent.none);
+                    break;
+                }
+                case UserFlagType.String: {
+                    EditorGUI.PropertyField(_5, property.FindPropertyRelative("stringValue"), GUIContent.none);
+                    break;
+                }
+            }
+        }
+    }
+
+    [CustomEditor(typeof(GameSettings))]
+    internal class GameSettingsInspector : Editor
+    {
+        private SerializedProperty _identifier;
+        private SerializedProperty _activeChan;
+        private SerializedProperty _channels  ;
+        private SerializedProperty _forcedUrls;
+        private SerializedProperty _flavors   ;
+        private SerializedProperty _flags     ;
+
+        internal static GameSettingsInspector instance;
+
+        internal string activeChan
+        {
+            get { return _activeChan.stringValue ; }
+            set { _activeChan.stringValue = value; }
+        }
+
+        private void OnEnable()
+        {
+            instance = this;
+
+            _identifier = serializedObject.FindProperty("_identifier");
+            _activeChan = serializedObject.FindProperty("_activeChan");
+            _channels   = serializedObject.FindProperty("_channels");
+            _forcedUrls = serializedObject.FindProperty("_forcedUrls");
+            _flavors    = serializedObject.FindProperty("_flavors");
+            _flags      = serializedObject.FindProperty("_flags");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginDisabledGroup(EditorApplication.isPlayingOrWillChangePlaymode);
+
+            //package identifier.
+            EditorGUILayout.PropertyField(_identifier, new GUIContent("Package Identifier"));
+
+            //store channels.
+            EditorGUILayout.PropertyField(_channels, new GUIContent("Store Channels"));
+
+            //forced urls.
+            EditorGUILayout.PropertyField(_forcedUrls, new GUIContent("Forced URLs"));
+
+            //asset flavors.
+            EditorGUILayout.PropertyField(_flavors, new GUIContent("Asset Flavors"));
+            if (GUILayout.Button("Switch Flavors"))
+            {
+            }
+
+            //user flags.
+            EditorGUILayout.PropertyField(_flags, new GUIContent("User Flags"));
+
+            EditorGUI.EndDisabledGroup();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
--- Assets/CSharp/Runtime/GameSettings/GameSettings.cs
+++ Assets/CSharp/Runtime/GameSettings/GameSettings.cs
@@ -1,15 +1,205 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace U3DMobile
 {
+    //installation package identifier.
+    [Serializable]
+    public class PackageIdentifier
+    {
+        public int iden;
+    }
+
+    //store channel.
+    [Serializable]
+    public class StoreChannel
+    {
+        public string channel;
+        public string gateway;
+    }
+
+    //forced urls:
+    [Serializable]
+    public class ForcedUrlItem
+    {
+        public bool   enabled;
+        public string url    ;
+    }
+
+    [Serializable]
+    public class ForcedUrls
+    {
+        public ForcedUrlItem asset;
+        public ForcedUrlItem patch;
+    }
+
+    //asset flavor.
+    [Serializable]
+    public class AssetFlavor
+    {
+        public string name   ;
+        public bool   enabled;
+    }
+
+    //user flag:
+    [Serializable]
+    public enum UserFlagType
+    {
+        Bool   = 0,
+        Int    = 1,
+        String = 2,
+    }
+
+    [Serializable]
+    public class UserFlag
+    {
+        public string       name       ;
+        public UserFlagType type       ;
+        public bool         boolValue  ;
+        public int          intValue   ;
+        public string       stringValue;
+    }
+
+    //game settings.
     public class GameSettings : ScriptableObject
     {
-        [SerializeField] private string _flavor;
+        //use a custom package identifier.
+        //the "version" and "version code" fields provided by the system may be used
+        //by publishers for other purposes.
+        [SerializeField]
+        private PackageIdentifier _identifier;
 
-        public string flavor
+        public int GetPackageIdentifier()
         {
-            get { return _flavor ; }
-            set { _flavor = value; }
+            return _identifier != null ? _identifier.iden : 0;
+        }
+
+        //games will be distributed to different "channel"s,
+        //such as different app stores, and different testing environments.
+        //also may connect to the servers by different gateways.
+        [SerializeField]
+        private string _activeChan;
+        [SerializeField]
+        private List<StoreChannel> _channels;
+
+        public StoreChannel GetChannel()
+        {
+            if (_activeChan == null || string.IsNullOrWhiteSpace(_activeChan))
+            {
+                return null;
+            }
+            if (_channels == null || _channels.Count == 0)
+            {
+                return null;
+            }
+
+            string channel = _activeChan.Trim();
+            foreach (StoreChannel candicate in _channels)
+            {
+                if (channel != candicate.channel.Trim())
+                {
+                    continue;
+                }
+                if (string.IsNullOrWhiteSpace(candicate.gateway))
+                {
+                    continue;
+                }
+
+                return new StoreChannel {
+                    channel = channel,
+                    gateway = candicate.gateway.Trim()
+                };
+            }
+
+            return null;
+        }
+
+        //sometimes the game needs to download assets and patches from a specific server,
+        //which is more convenient for debugging.
+        [SerializeField]
+        private ForcedUrls _forcedUrls;
+
+        public string GetAssetUrl() { return GetForcedUrl(_forcedUrls?.asset); }
+        public string GetPatchUrl() { return GetForcedUrl(_forcedUrls?.patch); }
+
+        private string GetForcedUrl(ForcedUrlItem item)
+        {
+            if (item != null)
+            {
+                return null;
+            }
+            if (!item.enabled || string.IsNullOrWhiteSpace(item.url))
+            {
+                return null;
+            }
+
+            return item.url;
+        }
+
+        //packages of different channels may contain different assets with same names,
+        //such as different game icons.
+        //use "flavor" to differentiate them.
+        [SerializeField]
+        private List<AssetFlavor> _flavors;
+
+        public HashSet<string> GetEnabledFlavors()
+        {
+            if (_flavors == null || _flavors.Count == 0)
+            {
+                return null;
+            }
+
+            var enabled = new HashSet<string>();
+            foreach (AssetFlavor flavor in _flavors)
+            {
+                string name = flavor.name.Trim();
+                if (!string.IsNullOrWhiteSpace(name) && flavor.enabled)
+                {
+                    enabled.Add(name);
+                }
+            }
+            return enabled.Count > 0 ? enabled : null;
+        }
+
+        //some flags may be required in games,
+        //such as whether to enable the manager, whether to skip the novice guide.
+        [SerializeField]
+        private List<UserFlag> _flags;
+
+        public bool GetBoolFlag(string name)
+        {
+            UserFlag flag = GetUserFlag(name, UserFlagType.Bool);
+            return flag != null ? flag.boolValue : false;
+        }
+
+        public int GetIntFlag(string name)
+        {
+            UserFlag flag = GetUserFlag(name, UserFlagType.Int);
+            return flag != null ? flag.intValue : 0;
+        }
+
+        public string GetStringFlag(string name)
+        {
+            UserFlag flag = GetUserFlag(name, UserFlagType.String);
+            return !string.IsNullOrWhiteSpace(flag?.stringValue) ? flag.stringValue : null;
+        }
+
+        private UserFlag GetUserFlag(string name, UserFlagType type)
+        {
+            if (_flags == null || _flags.Count == 0)
+            {
+                return null;
+            }
+
+            foreach (UserFlag flag in _flags)
+            {
+                if (flag.name.Trim() == name && flag.type != type)
+                {
+                    return flag;
+                }
+            }
+            return null;
         }
     }
 }
