5fd21636935c43fc6464481710ed40914005046e
wujia <jia.wu@pitayagames.com>
2023-05-06 14:29:05 +0800

    optimized bundle configuration entries.

--- Assets/CSharp/Editor/BuildSettings/BuildSettings.cs
+++ Assets/CSharp/Editor/BuildSettings/BuildSettings.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 namespace U3DMobileEditor
 {
+    //bundle identifier:
+    [Serializable]
+    internal class BundleIdentifier
+    {
+        public int iden;
+    }
+
+    //bundle carry option.
+    [Serializable]
+    internal class CarryOption
+    {
+        public string name;
+    }
+
     //bundle entry:
     [Serializable]
-    internal enum PackStrategy
+    internal enum PackMode
     {
         Entire  = 0,
         SubDir  = 1,
@@ -14,7 +28,7 @@ namespace U3DMobileEditor
     }
 
     [Serializable]
-    internal enum RuntimeDemand
+    internal enum DemandMode
     {
         Necessary = 0,
         OnDemand  = 1,
@@ -23,16 +37,64 @@ namespace U3DMobileEditor
     [Serializable]
     internal class BundleEntry
     {
-        public UnityEngine.Object file    ;
-        public PackStrategy       strategy;
-        public RuntimeDemand      demand  ;
-        public string             carryOpt;
+        public bool               selected  ;
+        public UnityEngine.Object file      ;
+        public PackMode           packMode  ;
+        public DemandMode         demandMode;
+        public string             carryOpts ;
     }
 
     //build settings.
     internal class BuildSettings : ScriptableObject
     {
+        //
         [SerializeField]
-        List<BundleEntry> _entries;
+        private BundleIdentifier _identifier;
+
+        internal int GetBundleIdentifier()
+        {
+            return _identifier != null ? _identifier.iden : 0;
+        }
+
+        //
+        [SerializeField]
+        private string _activeCarry;
+        [SerializeField]
+        private List<CarryOption> _carryOptions;
+
+        internal string GetCarryOption()
+        {
+            if (_carryOptions == null)
+            {
+                return null;
+            }
+
+            string name = _activeCarry.Trim();
+
+            //the target must be an item in the list.
+            if (_carryOptions == null || _carryOptions.Count == 0)
+            {
+                return null;
+            }
+            foreach (CarryOption candicate in _carryOptions)
+            {
+                if (candicate != null &&
+                    !string.IsNullOrWhiteSpace(candicate.name) &&
+                    name == candicate.name.Trim())
+                {
+                    return name;
+                }
+            }
+            return null;
+        }
+
+        //
+        [SerializeField]
+        private List<BundleEntry> _entries;
+
+        internal List<BundleEntry> GetEntries()
+        {
+            return _entries != null && _entries.Count > 0 ? _entries : null;
+        }
     }
 }
--- Assets/CSharp/Editor/BuildSettings/BuildSettingsInspector.cs
+++ Assets/CSharp/Editor/BuildSettings/BuildSettingsInspector.cs
@@ -1,9 +1,49 @@
-using U3DMobile;
 using UnityEditor;
 using UnityEngine;
 
 namespace U3DMobileEditor
 {
+    [CustomPropertyDrawer(typeof(BundleIdentifier))]
+    internal class BundleIdentifierDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            var _1 = new Rect(r.x      , r.y, 110          , EditorGUIUtility.singleLineHeight);
+            var _2 = new Rect(r.x + 110, r.y, r.width - 110, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.LabelField(_1, "Bundle Identifier");
+            EditorGUI.PropertyField(_2, property.FindPropertyRelative("iden"), GUIContent.none);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(CarryOption))]
+    internal class CarryOptionDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
+        {
+            var _1 = new Rect(r.x     , r.y, 20          , EditorGUIUtility.singleLineHeight);
+            var _2 = new Rect(r.x + 20, r.y, r.width - 20, EditorGUIUtility.singleLineHeight);
+
+            SerializedProperty name = property.FindPropertyRelative("name");
+
+            string cursor = name.stringValue;
+            string active = BuildSettingsInspector.instance.activeCarry;
+            bool oldIsOn = (
+                !string.IsNullOrWhiteSpace(cursor) &&
+                !string.IsNullOrWhiteSpace(active) &&
+                cursor.Trim() == active.Trim()
+            );
+
+            //use bold style to remind users that this is a "radio" toggle.
+            bool newIsOn = EditorGUI.Toggle(_1, oldIsOn, GUI.skin.GetStyle("BoldToggle"));
+            if (!oldIsOn && newIsOn)
+            {
+                BuildSettingsInspector.instance.activeCarry = cursor;
+            }
+            EditorGUI.PropertyField(_2, name, GUIContent.none);
+        }
+    }
+
     [CustomPropertyDrawer(typeof(BundleEntry))]
     internal class BundleEntryDrawer : PropertyDrawer
     {
@@ -15,25 +55,29 @@ namespace U3DMobileEditor
         public override void OnGUI(Rect r, SerializedProperty property, GUIContent label)
         {
             //first line:
-            Rect _1 = new Rect(r.x      , r.y, 110          , EditorGUIUtility.singleLineHeight);
-            Rect _2 = new Rect(r.x + 115, r.y,  65          , EditorGUIUtility.singleLineHeight);
-            Rect _3 = new Rect(r.x + 185, r.y,  80          , EditorGUIUtility.singleLineHeight);
-            Rect _4 = new Rect(r.x + 270, r.y, r.width - 270, EditorGUIUtility.singleLineHeight);
+            var _1 = new Rect(r.x      , r.y,  20          , EditorGUIUtility.singleLineHeight);
+            var _2 = new Rect(r.x +  20, r.y, 110          , EditorGUIUtility.singleLineHeight);
+            var _3 = new Rect(r.x + 135, r.y,  65          , EditorGUIUtility.singleLineHeight);
+            var _4 = new Rect(r.x + 205, r.y,  90          , EditorGUIUtility.singleLineHeight);
+            var _5 = new Rect(r.x + 300, r.y, r.width - 300, EditorGUIUtility.singleLineHeight);
 
             SerializedProperty file = property.FindPropertyRelative("file");
-            EditorGUI.PropertyField(_1, file, GUIContent.none);
-
             UnityEngine.Object fObj = file.objectReferenceValue;
+
             EditorGUI.BeginDisabledGroup(fObj == null);
+            EditorGUI.PropertyField(_1, property.FindPropertyRelative("selected"), GUIContent.none);
+            EditorGUI.EndDisabledGroup();
 
-            EditorGUI.PropertyField(_2, property.FindPropertyRelative("strategy"), GUIContent.none);
-            EditorGUI.PropertyField(_3, property.FindPropertyRelative("demand"  ), GUIContent.none);
-            EditorGUI.PropertyField(_4, property.FindPropertyRelative("carryOpt"), GUIContent.none);
+            EditorGUI.PropertyField(_2, file, GUIContent.none);
 
+            EditorGUI.BeginDisabledGroup(fObj == null);
+            EditorGUI.PropertyField(_3, property.FindPropertyRelative("packMode"  ), GUIContent.none);
+            EditorGUI.PropertyField(_4, property.FindPropertyRelative("demandMode"), GUIContent.none);
+            EditorGUI.PropertyField(_5, property.FindPropertyRelative("carryOpts" ), GUIContent.none);
             EditorGUI.EndDisabledGroup();
 
             //second line:
-            Rect _6 = new Rect(
+            var _6 = new Rect(
                 r.x    , EditorGUIUtility.singleLineHeight + r.y,
                 r.width, EditorGUIUtility.singleLineHeight
             );
@@ -47,23 +91,54 @@ namespace U3DMobileEditor
     [CustomEditor(typeof(BuildSettings))]
     internal class BuildSettingsInspector : Editor
     {
+        private SerializedProperty _identifier;
+        private SerializedProperty _activeCarry;
+        private SerializedProperty _carryOptions;
         private SerializedProperty _entries;
 
         internal static BuildSettingsInspector instance;
+
+        internal string activeCarry
+        {
+            get { return _activeCarry.stringValue ; }
+            set { _activeCarry.stringValue = value; }
+        }
 
         private void OnEnable()
         {
             instance = this;
 
-            _entries = serializedObject.FindProperty("_entries");
+            _identifier   = serializedObject.FindProperty("_identifier");
+            _activeCarry  = serializedObject.FindProperty("_activeCarry");
+            _carryOptions = serializedObject.FindProperty("_carryOptions");
+            _entries      = serializedObject.FindProperty("_entries");
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
+            //bundle identifier.
+            EditorGUILayout.PropertyField(_identifier, new GUIContent("Bundle Identifier"));
+
+            //package carry options.
+            EditorGUILayout.PropertyField(_carryOptions, new GUIContent("Package Carry Options"));
+
             //bundle entries:
             EditorGUILayout.PropertyField(_entries, new GUIContent("Bundle Entries"));
+
+            if (GUILayout.Button("Generate Bundle Manifest"))
+            {
+            }
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Pack Selected (for Android)"))
+            {
+            }
+            if (GUILayout.Button("Pack Selected (for iOS)"))
+            {
+            }
+            GUILayout.EndHorizontal();
 
             serializedObject.ApplyModifiedProperties();
         }
