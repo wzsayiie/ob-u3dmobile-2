0f4052db32b5b34d92c416abc73721ff3748b6ce
wujia <jia.wu@pitayagames.com>
2023-05-24 13:52:57 +0800

    added custom android manifest and activity.

---
+++ Assets/Plugins/Android/MainActivity.java
@@ -0,0 +1,37 @@
+package com.enterprise.app;
+
+import android.os.Bundle;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
